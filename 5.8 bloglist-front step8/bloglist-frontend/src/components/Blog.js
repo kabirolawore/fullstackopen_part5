475868e2771473ba43521f9c33c31096dac320b4
kabirolawore <horlaworey@gmail.com>
2023-03-27 14:53:21 +0100

    project 5.8 added

---
+++ 5.8 bloglist-front step8/bloglist-frontend/src/components/Blog.js
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+
+//
+const Blog = ({ blog, user }) => {
+  // const [detailVisible, setDetailVisible] = useState(false);
+  // const loginUser = user;
+  // console.log(loginUser);
+
+  // const blogStyle = {
+  //   paddingTop: 10,
+  //   paddingLeft: 2,
+  //   border: 'solid',
+  //   borderWidth: 1,
+  //   color: 'green',
+  //   marginBottom: 5,
+  // };
+  // // console.log(blog);
+
+  // const hideWhenNoDetail = { display: detailVisible ? 'none' : '' };
+  // const showWhenDetail = { display: detailVisible ? '' : 'none' };
+
+  // return (
+  //   <div style={blogStyle}>
+  //     <div style={hideWhenNoDetail}>
+  //       {blog.title} {blog.author}{' '}
+  //       <button onClick={() => setDetailVisible(true)}>view</button>
+  //     </div>
+  //     <div style={showWhenDetail}>
+  //       {blog.title} {blog.author}{' '}
+  //       <button onClick={() => setDetailVisible(false)}>hide</button>
+  //       <div>{blog.url}</div>
+  //       <div>{blog.likes}</div>
+  //       <div>{blog.user.name}</div>
+  //     </div>
+  //   </div>
+  // );
+
+  const [isHidden, setIsHidden] = useState(true);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    color: 'green',
+    marginBottom: 5,
+  };
+  // console.log(blog);
+
+  const toggleDetails = () => {
+    setIsHidden(!isHidden);
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}{' '}
+        <button onClick={toggleDetails}>{isHidden ? 'view' : 'hide'}</button>
+      </div>
+      {!isHidden && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            {blog.likes} <button>likes</button>
+          </div>
+          <div>{blog.user.name ? blog.user.name : user}</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;

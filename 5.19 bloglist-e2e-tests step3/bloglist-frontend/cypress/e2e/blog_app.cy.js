d8e4d46abdaa3b4eceb91d82f327e7239b69b19f
kabirolawore <horlaworey@gmail.com>
2023-05-14 20:42:49 +0100

    5.19 project added

---
+++ 5.19 bloglist-e2e-tests step3/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -0,0 +1,69 @@
+describe('Blog App', () => {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset');
+
+    const user = {
+      name: 'kabir',
+      username: 'admin',
+      password: 'admin',
+    };
+
+    cy.request('POST', 'http://localhost:3003/api/users/', user);
+    cy.visit('http://localhost:3000');
+  });
+
+  it('Login form is shown', function () {
+    // cy.contains('Log in to Application');
+    // cy.contains('username');
+    // cy.contains('password');
+    cy.get('html')
+      .should('contain', 'Log in to Application')
+      .and('contain', 'username')
+      .and('contain', 'password')
+      .and('contain', 'login');
+  });
+
+  describe('Login', function () {
+    //
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('admin');
+      cy.get('#password').type('admin');
+      cy.get('#login-button').click();
+
+      cy.contains('logout');
+    });
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('admin');
+      cy.get('#password').type('wrong');
+      cy.get('#login-button').click();
+
+      cy.get('.error')
+        .should('contain', 'Wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)');
+    });
+  });
+
+  describe('When logged in', function () {
+    beforeEach(function () {
+      cy.get('#username').type('admin');
+      cy.get('#password').type('admin');
+      cy.get('#login-button').click();
+      cy.contains('create new blog').click();
+    });
+
+    it('A blog can be created', function () {
+      //
+      cy.get('#title').type('a test title');
+      cy.get('#author').type('kabir');
+      cy.get('#url').type('http://test.com');
+
+      cy.get('#create-button').click();
+      cy.get('.success')
+        .should('contain', 'a test title by kabir added')
+        .and('have.css', 'color', 'rgb(0, 128, 0)');
+    });
+
+    // it('users can like a blog', function () {});
+  });
+});
